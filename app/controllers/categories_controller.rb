cf47aac45f3c727fd673429c0293230df42bd577
danainjax <danainjax@gmail.com>
2021-04-09 19:11:27 -0400

    commit tree a6b56d0234bdbe2aec8dead772e13664a18aaeaa

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,27 @@
 class CategoriesController < ApplicationController
 
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Categories.all 
+  end
+
+  def create
+    @category = Category.create(category_params)
+    if @comment.save
+      redirect_to category_path(category)
+    end
+  end
+
+
   def show
     @category = Category.find(params[:id])
+  end
+
+
+  def category_params
+    params.require(:category).require(:name)
   end
 end
